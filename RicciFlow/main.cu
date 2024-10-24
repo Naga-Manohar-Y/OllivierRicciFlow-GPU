2ac592fb60a03e39a1c4a5a137bcca719dd375bf
akhlaque <akhlaque.ak@gmail.com>
2024-10-24 17:54:33 -0400

    updating

--- RicciFlow/main.cu
+++ RicciFlow/main.cu
@@ -10,6 +10,8 @@ int main(int argc, char* argv[]) {
     }
     cout<<endl;
     floyd_warshall_kernel<<<BLK_NUMS, BLK_DIM>>>(g);
+    cudaDeviceSynchronize();
+
     chkerr(cudaMemcpy(g->offset, g->d_offset, g->n * sizeof(ui), cudaMemcpyDeviceToHost));
     for(ui i=0;i<g->n;i++){
         cout<<g->offset[i]<<" ";
