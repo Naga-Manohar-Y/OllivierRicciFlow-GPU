af26fb216fe6dbcfd4f508c83cbff079c917bd12
akhlaque <akhlaque.ak@gmail.com>
2024-10-24 18:02:29 -0400

    updating

--- RicciFlow/main.cu
+++ RicciFlow/main.cu
@@ -6,11 +6,12 @@ int main(int argc, char* argv[]) {
     cudaMallocManaged(&g, sizeof(GPUGraph));
     g->readBinaryFile(argv[1]);
     g->copyToGPU();
+
     for(ui i=0;i<g->n;i++){
         cout<<g->offset[i]<<" ";
     }
     cout<<endl;
-    cudaDeviceSynchronize();
+
     floyd_warshall_kernel<<<BLK_NUMS, BLK_DIM>>>(g);
     cudaDeviceSynchronize();
     cout<<"n: "<<g->n<<endl;
