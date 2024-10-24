35d36886edd963430283c8b2ecdd2d406d365f6b
akhlaque <akhlaque.ak@gmail.com>
2024-10-24 17:29:55 -0400

    updating

--- RicciFlow/main.cu
+++ RicciFlow/main.cu
@@ -5,5 +5,6 @@ int main(int argc, char* argv[]) {
     GPUGraph *g;
     cudaMallocManaged(&g, sizeof(GPUGraph));
     g->readBinaryFile(argv[1]);
+    floyd_warshall_kernel<<<BLK_NUMS, BLK_DIM>>>(g);
     return 0;
 }
