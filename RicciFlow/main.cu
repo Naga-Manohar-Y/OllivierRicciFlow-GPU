b20a95640a9ff234dc7ceacb636ae2f3a9bdb2cb
akhlaque <akhlaque.ak@gmail.com>
2024-10-25 16:00:27 -0400

    updating

--- RicciFlow/main.cu
+++ RicciFlow/main.cu
@@ -1,7 +1,7 @@
 #include "common.h"
 #include "graph.h"
 #include "ATD.h"
-__global__ float* gsum;
+__device__ float* gsum;
 
 __global__ void d_update_weights(GPUGraph* g, float* edge_RC){
     float sumw = 0;
