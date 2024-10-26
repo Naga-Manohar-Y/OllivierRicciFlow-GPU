7ec3e3a80e807c1f47eaa5ce06ccc89060d77c50
akhlaque <akhlaque.ak@gmail.com>
2024-10-25 21:45:55 -0400

    updating

--- RicciFlow/main.cu
+++ RicciFlow/main.cu
@@ -65,7 +65,7 @@ public:
         }
         cudaDeviceSynchronize();
         float *weights=new float[M];
-        chkerr(cudaMemcpy(weights, g->d_weights, 100 * sizeof(float), cudaMemcpyDeviceToHost));
+        chkerr(cudaMemcpy(weights, g->d_weights, 1 * sizeof(float), cudaMemcpyDeviceToHost));
         for(ui i=0;i<M;i++){
             cout<<weights[i]<<" ";
         }
